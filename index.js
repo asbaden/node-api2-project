53bbaba922f8a8f2888ae21d2cbfe3d3e10dfaa2
Gus Baden <gusbaden96@gamil.com>
2020-02-13 10:37:25 -0800

    fixed port

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ server.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
-  });
+});
   
